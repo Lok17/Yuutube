bdbb83551f16c25a9e0a88b8d4a7fa3e8bab9c12
Lok17 <rotabite987@gmail.com>
2021-01-05 21:50:11 +0800

    Yuutube (download)

---
+++ Yuutube/src/yuutube/Login.java
@@ -0,0 +1,61 @@
+package yuutube;
+
+import java.util.Scanner;
+
+public class Login {
+	private String email;
+	private String password;
+	private String name;
+	private String id;
+	private boolean in = false;
+	Scanner sc = new Scanner(System.in);
+
+	public Login() {
+		System.out.print("Email: ");
+		email = sc.nextLine();
+		System.out.print("Password: ");
+		password = sc.nextLine();
+		// 2fa method
+		// check password method
+		if (MySQL.checkPassword(email, password)) {
+			System.out.println("Wait a minute, we are sending you the confirmation code...");
+			int code = TwoFA.ConfirmLogin(email);
+			System.out.println("We have sent you the confirmation code, please check your email");
+			System.out.print("Code: ");
+			if (Integer.toString(code).equals(sc.nextLine())) {
+				System.out.println("Login success!");
+				String[] s = MySQL.getUserInfo(email);
+				id = s[0];
+				name = s[1];
+				in = true;
+
+
+				// name,id
+			} else {
+				System.out.println("Login failed. Please try again.");
+			}
+
+		} else {
+			System.out.println("Sorry! Invalid login email or password.");
+		}
+		
+
+	}
+
+	public boolean isIn() {
+		return in;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+}
---
+++ Yuutube/src/yuutube/MySQL.java
@@ -0,0 +1,143 @@
+package yuutube;
+
+import java.sql.*;
+
+public class MySQL {
+	private static final String url = "jdbc:mysql://localhost:3306/test?zeroDateTimeBehavior=CONVERT_TO_NULL"; // MySQL username
+	private static final String user = "root";// MySQL password
+	private static final String password = "12345678";
+
+	public MySQL() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			// System.out.println("Success!");
+
+		} catch (Exception e) {
+			System.out.println("Fail!");
+			// e.printStackTrace();
+		}
+	}
+
+	static boolean emailExist(String email) {
+		try {
+			Connection com;
+			com = DriverManager.getConnection(url, user, password);
+//			if(!com.isClosed()) {
+//			} 
+			Statement statement = com.createStatement();
+			String sql = "USE test";
+			statement.executeQuery(sql);
+			// the following is for my database
+			ResultSet rs = statement.executeQuery("SELECT email FROM users WHERE email='" + email + "';");
+
+			if (rs.next()) {
+				return true;
+			}
+			return false;
+
+		} catch (Exception e) {
+			// System.out.println("error!");
+			// e.printStackTrace();
+		}
+		return false;
+	}
+
+	static boolean checkPassword(String email, String pass) {
+		try {  
+			Connection com;
+			com = DriverManager.getConnection(url, user, password);
+//			if(!com.isClosed())
+//				System.out.println("connection successful!");
+			Statement statement = com.createStatement();
+
+			String sql = "USE test";
+			statement.executeQuery(sql);
+			// the following is for my database
+			ResultSet rs = statement.executeQuery("SELECT password FROM users WHERE email='" + email + "';");
+			if (rs.next()) {
+				String dbpass = rs.getString("password");
+				if (pass.equals(dbpass)) {
+					return true;
+				}
+
+			}
+			return false;
+
+		} catch (Exception e) {
+//			System.out.println("error!");
+//			e.printStackTrace();
+		}
+		return false;
+	}
+
+	static void registerUser(String email, String name, String pass) {
+		try {
+			Connection com;
+			com = DriverManager.getConnection(url, user, password);
+//			if(!com.isClosed())
+//				System.out.println("connection successful!");
+			Statement statement = com.createStatement();
+
+			String sql = "USE test";
+			statement.executeQuery(sql);
+			// the following is for my database
+                        statement.executeUpdate("INSERT INTO users (email,password,user_name)VALUES(\"" + email + "\",\"" + pass
+					+ "\",\"" + name + "\");");
+			// System.out.println();
+
+		} catch (Exception e) {
+			// System.out.println("error!");
+			// e.printStackTrace();
+		}
+
+	}
+
+	static void resetPassword(String email, String pass) {
+		try {
+			Connection com;
+			com = DriverManager.getConnection(url, user, password);
+//			if(!com.isClosed())
+//				System.out.println("connection successful!");
+			Statement statement = com.createStatement();
+
+			String sql = "USE test";
+			statement.executeQuery(sql);
+			// the following is for my database
+			statement.executeUpdate(
+					"UPDATE users SET password = '"+pass+"' WHERE email = '"+email + "';");
+			// System.out.println();
+
+		} catch (Exception e) {
+			// System.out.println("error!");
+			// e.printStackTrace();
+		}
+
+	}
+
+	static String[] getUserInfo(String email) {
+		String id = null, name = null;
+		try {
+			Connection com;
+			com = DriverManager.getConnection(url, user, password);
+//			if(!com.isClosed())
+//				System.out.println("connection successful!");
+			Statement statement = com.createStatement();
+
+			String sql = "USE test";
+			statement.executeQuery(sql);
+
+			ResultSet rs = statement.executeQuery("SELECT user_id, user_name FROM users WHERE email='" + email + "';");
+			if (rs.next()) {
+				id = rs.getString("user_id");
+				name = rs.getString("user_name");
+			}
+
+		} catch (Exception e) {
+			// System.out.println("error!");
+			// e.printStackTrace();
+		}
+		return new String[] { id, name };
+
+	}
+
+}
